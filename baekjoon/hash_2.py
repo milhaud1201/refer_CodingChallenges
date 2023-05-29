ecc9576b5134a3927eb7e3ce91ab982da659530f
yelimlikelion <pizzamini@likelion.org>
2023-05-29 15:10:09 +0900

    programmers_lv1_1hr

---
+++ baekjoon/hash_2.py
@@ -0,0 +1,15 @@
+def solution(participant, completion):
+    participant_dict = {}
+    for name in participant:
+        if name in participant_dict:
+            participant_dict[name] += 1
+        else:
+            participant_dict[name] = 1
+    for name in completion:
+        if name in participant_dict:
+            participant_dict[name] -= 1
+        else:
+            return name
+    for name, count in participant_dict.items():
+        if count != 0:
+            return name
